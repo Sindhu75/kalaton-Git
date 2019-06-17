fbc0b952f8dfe9ca31c1e1096a605003be4b68bb
Sindhu Ravilala <sindhu.ravilala@testingxperts.loc>
2019-06-17 11:30:42 +0530

    test message

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>917d8943-e904-427e-b4b7-5a72aa86ff01</testSuiteGuid>
+   <testCaseLink>
+      <guid>a0f6a6e9-4744-40fa-8af1-17e7f990909f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1 by Tom</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3f1550eb-bf4d-4663-900a-7252415d8f08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1by kim</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
